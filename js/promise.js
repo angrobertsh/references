dbb6a94a00b2c53e826a971920e789c729e95a6b
Robert Ang <angrobertsh@gmail.com>
2017-02-24 01:41:41 -0800

    found some jquery versions

--- js/promise.js
+++ js/promise.js
@@ -2,6 +2,8 @@
 Promise.resolve(function1()).then(function2());
 Promise.resolve(this.updateBounds(bounds)).then(this.requestAllBathrooms(this.props.filters));
 
+// example2
+
 const myFirstPromise = new Promise((resolve, reject) => {
     setTimeout( () => {resolve("Success!")}, 250);
 });
@@ -11,3 +13,43 @@ myFirstPromise.then( (successMessage) => {
 });
 
 // jQuery version because Promise isn't supported in IE
+
+const function1 = () => {
+  const deferred = $.Deferred();
+  function1action(deferred.resolve)
+  return deferred.promise()
+}
+
+function1().then(function2());
+
+// example 2
+
+let cache = {};
+
+const getData = (val) => {
+    // return either the cached value or jqXHR object wrapped Promise
+    return $.when(
+        cache[ val ] ||
+        $.ajax('/foo/', {
+            data: { value: val },
+            dataType: 'json',
+            success: function( resp ){
+                cache[ val ] = resp;
+            }
+        })
+    );
+}
+
+getData('foo').then((resp) => {
+    // do something with the response, which may
+    // or may not have been retrieved using an
+    // XHR request.
+});
+
+// two possibilities
+
+$.when(this.updateBounds(bounds)).then((resp) => {
+  this.requestAllBathrooms(filter)
+})
+
+$.when(this.updateBounds(bounds)).then(this.requestAllBathrooms(filter));
