71aa1482508bd7cfcce5f07c5dc371b62d258640
Robert Ang <angrobertsh@gmail.com>
2017-05-18 20:11:40 -0700

    added some rails stuff

---
+++ js/decomposition_and_ternary.js
@@ -0,0 +1,6 @@
+// creates appropriate variables
+const { north, south, east, west } = {east: 8, north: 3, south: 4, west: 9};
+
+// ternary
+const size = 8;
+const poo = size > 2 ? "bigpoop" : "smallpoop";
---
+++ rails/assets.md
@@ -0,0 +1,5 @@
+Put assets in the appropriate folder (images, whatever).
+
+Access in CSS with
+
+background-image: image-url("filename.png");
